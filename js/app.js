48c9f700ae0e09237e57d9d01b50c28d3b920897
pjsajama <pjsajama@gmail.com>
2024-03-20 01:55:13 -0300

    Ultimas revisiones de los estilos y javascript

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 const campo_texto = document.getElementById("caja-texto");
 const campo_mensaje = document.getElementById("texto-resultado");
 
+const txt_encontrado1 = document.getElementById("texto-resultado");
+const txt_encontrado2 = document.getElementById("btn-copiar");
+const txt_no_encontrado1 = document.getElementById("muneco");
+const txt_no_encontrado2 = document.getElementById("cartel1");
+const txt_no_encontrado3 = document.getElementById("cartel2");
+
 const matriz_code = [ ["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"] ];
 
 function btnEncriptar() {
-    let texto = encriptar(campo_texto.value);
-    console.log(texto);
+    let texto = encriptar(campo_texto.value.toLowerCase());
     campo_mensaje.value = texto;
+    mostrar();
 }
 
 function btnDesencriptar() {
-    let texto = desencriptar(campo_texto.value);
-    console.log(texto);
-    campo_mensaje.value = texto 
+    let texto = desencriptar(campo_texto.value.toLowerCase());
+    campo_mensaje.value = texto;
+    mostrar();
 }
 
 function encriptar(fraseEncriptada) {
@@ -36,4 +42,24 @@ function desencriptar(fraseEncriptada) {
 function btnCopiar() {
     campo_mensaje.select();
     navigator.clipboard.writeText(campo_mensaje.value);
+    no_mostrar();
+}
+
+function mostrar() {
+    txt_encontrado1.style.display = "block";
+    txt_encontrado2.style.display = "block";
+    txt_no_encontrado1.style.display = "none";
+    txt_no_encontrado2.style.display = "none";
+    txt_no_encontrado3.style.display = "none";
+    campo_texto.value = "";
+}
+
+function no_mostrar() {
+    txt_encontrado1.style.display = "none";
+    txt_encontrado2.style.display = "none";
+    txt_no_encontrado1.style.display = "block";
+    txt_no_encontrado2.style.display = "block";
+    txt_no_encontrado3.style.display = "block";
+    campo_mensaje.value = "";
+    //alert("EL TEXTO ENCRIPTADO COPIADO SE BORRARÁ")
 }
